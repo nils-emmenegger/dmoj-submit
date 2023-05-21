0c25b58ce922f4321001ae87e84977684742dadb
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-05-21 01:41:21 -0400

    Split code into subcommands and api modules

---
+++ src/api.rs
@@ -0,0 +1,132 @@
+use anyhow::{anyhow, Context, Result};
+use serde::Deserialize;
+
+pub const BASE_URL: &str = "https://dmoj.ca";
+
+#[allow(dead_code)]
+/// DMOJ API response
+#[derive(Deserialize, Debug)]
+pub struct APIResponse<T> {
+    pub api_version: String,
+    pub method: String,
+    pub fetched: String,
+    pub data: Option<T>,
+    pub error: Option<APIErrorFormat>,
+}
+
+#[allow(dead_code)]
+/// DMOJ API data format for a single object
+#[derive(Deserialize, Debug)]
+pub struct APISingleData<T> {
+    pub object: T,
+}
+
+#[allow(dead_code)]
+/// DMOJ API data format for lists of objects
+#[derive(Deserialize, Debug)]
+pub struct APIListData<T> {
+    pub current_object_count: i32,
+    pub objects_per_page: i32,
+    pub total_objects: i32,
+    pub page_index: i32,
+    pub total_pages: i32,
+    pub has_more: bool,
+    pub objects: Vec<T>,
+}
+
+#[allow(dead_code)]
+/// DMOJ API error format
+#[derive(Deserialize, Debug)]
+pub struct APIErrorFormat {
+    pub code: i32,
+    pub message: String,
+}
+
+#[allow(dead_code)]
+/// DMOJ API /api/v2/languages format
+#[derive(Deserialize, Debug)]
+pub struct APILanguage {
+    pub id: i32,
+    pub key: String,
+    pub short_name: Option<String>,
+    pub common_name: String,
+    pub ace_mode_name: String,
+    pub pygments_name: String,
+    pub code_template: String,
+}
+
+#[allow(dead_code)]
+/// DMOJ API /api/v2/submission/<submission id> format
+#[derive(Deserialize, Debug)]
+pub struct APISubmission {
+    pub id: i32,
+    pub problem: String,
+    pub user: String,
+    pub date: String,
+    pub time: Option<f64>,
+    pub memory: Option<f64>,
+    pub points: Option<f64>,
+    pub language: String,
+    pub status: String,
+    pub result: Option<String>,
+    pub case_points: f64,
+    pub case_total: f64,
+    pub cases: Vec<APISubmissionCaseOrBatch>,
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum APISubmissionCaseOrBatch {
+    Case(APISubmissionCase),
+    Batch(APISubmissionBatch),
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+pub struct APISubmissionCase {
+    pub r#type: String,
+    pub case_id: i32,
+    pub status: String,
+    pub time: f64,
+    pub memory: f64,
+    pub points: f64,
+    pub total: f64,
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+pub struct APISubmissionBatch {
+    pub r#type: String,
+    pub batch_id: i32,
+    pub cases: Vec<APISubmissionCase>,
+    pub points: f64,
+    pub total: f64,
+}
+
+pub fn get_languages() -> Result<Vec<APILanguage>> {
+    let json: APIResponse<APIListData<APILanguage>> =
+        reqwest::blocking::get(format!("{BASE_URL}/api/v2/languages"))
+            .with_context(|| "API request failed")?
+            .json()
+            .with_context(|| "converting API response to json failed")?;
+    if let Some(error) = json.error {
+        Err(anyhow!(
+            "API request failed with code {} and message `{}`",
+            error.code,
+            error.message
+        ))
+    } else if let Some(data) = json.data {
+        if data.has_more {
+            // TODO: fix this
+            log::error!(
+                "There is more than one page of languages, but we are only reading the first one"
+            );
+        }
+        Ok(data.objects)
+    } else {
+        Err(anyhow!(
+            "Neither data nor error were defined in the API response"
+        ))
+    }
+}
---
+++ src/subcommands.rs
@@ -0,0 +1,153 @@
+use crate::api::*;
+use anyhow::{anyhow, Context, Result};
+use reqwest::header::AUTHORIZATION;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+
+pub fn submit(problem: &str, source: &str, token: &str, language: &str) -> Result<()> {
+    // make a map of language keys to language ids
+    let key_id_map = get_languages()?
+        .into_iter()
+        .map(|lang| (lang.key.to_lowercase(), lang.id))
+        .collect::<HashMap<String, i32>>();
+    let lang_id = key_id_map
+        .get(&language.to_lowercase())
+        .with_context(|| "could not determine language id")?;
+
+    let header = format!("Bearer {}", token);
+    let url = format!("{}/problem/{}/submit", BASE_URL.to_string(), problem);
+    let params = [
+        ("problem", problem),
+        ("source", source),
+        ("language", &lang_id.to_string()),
+    ];
+    // Need some concurrency primitives here to appease the compiler
+    let redirect_url = Arc::new(Mutex::new(None));
+    let client = {
+        let redirect_url_clone = Arc::clone(&redirect_url);
+        reqwest::blocking::Client::builder()
+            .redirect(reqwest::redirect::Policy::custom(move |attempt| {
+                *redirect_url_clone.lock().unwrap() = Some(attempt.url().clone());
+                attempt.stop()
+            }))
+            .build()
+    }?;
+    log::info!("Fetching {} ...", url);
+    let submission = client
+        .post(&url)
+        .form(&params)
+        .header(AUTHORIZATION, &header)
+        .send()?;
+
+    let redirect_url = redirect_url
+        .lock()
+        .unwrap()
+        .clone()
+        .with_context(|| "Submission request did not get redirected to the submission page")?;
+    let res = submission.status().as_u16();
+    // TODO: figure out wonkiness with POST codes to make sure it does not break the below code block
+    if res != 302 {
+        return match res {
+            400 => Err(anyhow!(
+                "Error 400, bad request, the header you provided is invalid"
+            )),
+            401 => Err(anyhow!(
+                "Error 401, unauthorized, the token you provided is invalid"
+            )),
+            403 => Err(anyhow!(
+                "Error 403, forbidden, you are trying to access the admin portion of the site"
+            )),
+            404 => Err(anyhow!("Error 404, not found, the problem does not exist")),
+            500 => Err(anyhow!("Error 500, internal server error")),
+            code => Err(anyhow!("Code {code}, unknown network error")),
+        };
+    }
+    log::info!("submission url: {}", redirect_url);
+    let submission_id = redirect_url
+        .as_str()
+        .split('/')
+        .last()
+        .with_context(|| "could not determine submission id")?;
+    log::info!("submission id: {}", submission_id);
+
+    // https://github.com/DMOJ/online-judge/blob/master/judge/models/submission.py
+    // from https://github.com/DMOJ/online-judge/blob/81f3c90ffad12586f9edc9e17c8aa0bd66f28ecc/judge/models/submission.py#L49
+    const USER_DISPLAY_CODES_TUPLES: [(&str, &str); 15] = [
+        ("AC", "Accepted"),
+        ("WA", "Wrong Answer"),
+        ("SC", "Short Circuited"),
+        ("TLE", "Time Limit Exceeded"),
+        ("MLE", "Memory Limit Exceeded"),
+        ("OLE", "Output Limit Exceeded"),
+        ("IR", "Invalid Return"),
+        ("RTE", "Runtime Error"),
+        ("CE", "Compile Error"),
+        ("IE", "Internal Error (judging server error)"),
+        ("QU", "Queued"),
+        ("P", "Processing"),
+        ("G", "Grading"),
+        ("D", "Completed"),
+        ("AB", "Aborted"),
+    ];
+    let user_display_codes_map: HashMap<String, String> = HashMap::from_iter(
+        USER_DISPLAY_CODES_TUPLES
+            .into_iter()
+            .map(|(key, val)| (key.to_string(), val.to_string())),
+    );
+    let get_display = |code: &str| {
+        user_display_codes_map
+            .get(code)
+            .with_context(|| "unknown display code")
+    };
+
+    let client = reqwest::blocking::Client::new();
+    loop {
+        // TODO: add more logging
+        let json: APIResponse<APISingleData<APISubmission>> = client
+            .get(format!("{BASE_URL}/api/v2/submission/{submission_id}"))
+            .header(AUTHORIZATION, &header)
+            .send()?
+            .json()
+            .with_context(|| "converting API response to json failed")?;
+        // TODO: maybe add a dmoj_json_unwrap function that encapsulates the
+        // if let Some(error) = json.error ... else if let Some(data) = json.data ... else return err
+        // form and just returns a Result with successful data.
+        // Right now this form is copied/repeated in get_languages.
+        if let Some(error) = json.error {
+            return Err(anyhow!(
+                "API request failed with code {} and message `{}`",
+                error.code,
+                error.message
+            ));
+        } else if let Some(data) = json.data {
+            if let Some(result) = data.object.result {
+                // Submission has finished grading
+                println!(
+                    "Submission finished with result {}",
+                    get_display(result.as_str())?
+                );
+                break;
+            } else {
+                // Submission has not finished grading
+                println!("Status {}", get_display(data.object.status.as_str())?);
+            }
+        } else {
+            return Err(anyhow!(
+                "Neither data nor error were defined in the API response"
+            ));
+        }
+        std::thread::sleep(Duration::from_secs(1));
+    }
+    Ok(())
+}
+
+pub fn list_languages() -> Result<()> {
+    let mut print_lang_list = get_languages()?
+        .into_iter()
+        .map(|lang| format!("{}: {}", lang.common_name, lang.key.to_lowercase()))
+        .collect::<Vec<String>>();
+    print_lang_list.sort_unstable();
+    println!("{}", print_lang_list.join("\n"));
+    Ok(())
+}
